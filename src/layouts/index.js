7c8f0fd7887f7ba31dde8e89b9bf9a08d414e6c4
James Lutley <james@jameslutley.com>
2017-10-16 17:01:01 +0700

    added netlify-cms admin page and authentication

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -55,6 +55,17 @@ const TemplateWrapper = ({ children }) => (
       {children()}
     </div>
   </div>
+  <script>
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  </script>
 )
 
 TemplateWrapper.propTypes = {
