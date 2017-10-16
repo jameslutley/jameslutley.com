8334961ea457d784e3482d6ca5f99552f3b45594
James Lutley <james@jameslutley.com>
2017-10-16 18:55:06 +0700

    added normalize.css and typekit fonts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,12 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import { styled, injectGlobal } from 'styled-components'
+import { normalize } from 'polished'
 
-import './index.css'
+// import './index.css'
+
+const Normalize = injectGlobal`${normalize()}`
 
 const Header = () => (
   <div
     style={{
+      fontFamily: 'objektiv-mk1',
       background: 'rebeccapurple',
       marginBottom: '1.45rem',
     }}
@@ -37,10 +42,13 @@ const Header = () => (
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="James Lutley — Designer, Developer, Maker"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
+      ]}
+      link={[
+        { rel: 'stylesheet', href: 'https://use.typekit.net/uon4clj.css' }
       ]}
     />
     <Header />
