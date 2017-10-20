f61f501f35a724f3943966bb5f26134e2a08892d
James Lutley <james@jameslutley.com>
2017-10-20 17:00:21 +0700

    added fields to gatsby-node for building blog pages with images

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,23 +17,33 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators
   return new Promise((resolve, reject) => {
     graphql(`
-        {
-          allMarkdownRemark {
-            edges {
-              node {
-                fields {
-                  slug
-                }
+      {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
+          edges {
+            node {
+              id
+              html
+              excerpt
+              timeToRead
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                path
+                date(formatString: "D MMMM, YYYY")
+                rawDate: date
+                categories
               }
             }
           }
         }
-      `
+      }`
     ).then(result => {
       result.data.allMarkdownRemark.edges.map(({ node }) => {
         createPage({
           path: node.fields.slug,
-          component: path.resolve(`./src/templates/blog-post.js`),
+          component: path.resolve(`src/templates/blog-post.js`),
           context: {
             // Data passed to context is available in page queries as GraphQL variables.
             slug: node.fields.slug,
